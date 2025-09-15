0d2fd0f65184f94da002a4d10b8b342f7fcd035b
Subin-Kumar <137891964+Subin-Kumar@users.noreply.github.com>
2025-09-15 11:52:48 +0530

    Changed IP

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@
 const PRISMA_PORT = 3009
 // export const prismaBaseApi = `http://3.111.142.178:${PRISMA_PORT}`
 // export const prismaBaseApi = `http://192.168.1.11:${PRISMA_PORT}`
-export const prismaBaseApi = `http://localhost:${PRISMA_PORT}`
+export const prismaBaseApi = `http://65.0.182.176:${PRISMA_PORT}`
+// export const prismaBaseApi = `http://localhost:${PRISMA_PORT}`
